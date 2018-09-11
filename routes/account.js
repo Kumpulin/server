a0ccfdba0bc6d580306d95987ad249bdecb471f0
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-11 10:25:33 +0700

    Add more routes related to events

--- routes/account.js
+++ routes/account.js
@@ -3,8 +3,11 @@ const router = express.Router()
 
 const AccountController = require('../controllers/AccountController')
 
-router.post('/update_profile', AccountController.updateProfile)
+router.get('/joined', AccountController.getAllJoinedEvents)
+router.get('/attended', AccountController.getAllAttendedEvents)
+
 router.get('/current_user', AccountController.getCurrentUser)
+router.post('/update_profile', AccountController.updateProfile)
 router.post('/change_password', AccountController.changePassword)
 
 module.exports = router
