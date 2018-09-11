c8441d95af6f94bc7a9dc686b1c10d188671a9c8
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-11 10:58:36 +0700

    Rename relation

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ class User extends Model {
     const Event = require('./Event')
 
     return {
-      events: {
+      createdEvents: {
         relation: Model.HasManyRelation,
         modelClass: Event,
         join: {
