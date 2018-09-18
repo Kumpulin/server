1e36f3fc41dcbf5e3e9afad246b05c60f4b3b3db
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-18 11:32:18 +0700

    Remove fileFilter & fix export

--- config/multer.js
+++ config/multer.js
@@ -1,27 +1,14 @@
 const multer = require('multer')
+const path = require('path')
 
-exports.default = {
-  storage: multer.diskStorage({
-    destination: (req, file, next) => {
-      next(null, 'public/images/uploads')
-    },
+module.exports = multer.diskStorage({
+  destination: (req, file, next) => {
+    next(null, path.join(__dirname, '..', 'public/images/uploads'))
+  },
 
-    filename: (req, file, next) => {
-      const ext = file.mimetype.split('/')[1]
+  filename: (req, file, next) => {
+    const ext = file.mimetype.split('/')[1]
 
-      next(null, `${file.fieldname}-${Date.now()}.${ext}`)
-    }
-  }),
-
-  fileFilter: (req, file, next) => {
-    if (!file) return next()
-
-    const image = file.mimetype.startsWith('image/')
-
-    if (image) {
-      next(null, true)
-    } else {
-      return next({ message: 'File not supported.' })
-    }
+    next(null, `${file.fieldname}-${Date.now()}.${ext}`)
   }
-}
+})
